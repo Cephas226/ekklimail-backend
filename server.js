8b70164c25cd12850db44f894aeee097b330291a
Cephas <cephaszoubga@gmail.com>
2020-10-16 16:47:37 +0100

    ok

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ var async = require("async");
 const mongoose = require("mongoose");
 
 server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ekklimail.web.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 var corsOptions = {
-  origin: 'https://ekklimail.web.app',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 var uploadVar;
