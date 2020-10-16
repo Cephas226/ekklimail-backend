501fb9405a4cd4c3ee2374eea40b3445d3ef1f11
Cephas <cephaszoubga@gmail.com>
2020-10-16 15:14:34 +0100

    ok

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const mongoose = require("mongoose");
 
 server.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', 'https://enigmatic-reef-10449.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
