9faf94ae332553f9548b6c16286dd000885f18fb
Cephas <cephaszoubga@gmail.com>
2020-10-16 15:51:35 +0100

    ok

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ server.use(bodyParser.json());
 var async = require("async");
 const mongoose = require("mongoose");
 
-server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ekklimail.web.app');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 var uploadVar;
